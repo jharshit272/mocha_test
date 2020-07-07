785d21900c110bc85bbe82cd12d33cf31ca90983
Harshit Jain <HJAIN@amfam.com>
2020-07-06 23:12:43 -0500

    commit

---
+++ test/appTest.js
@@ -0,0 +1,27 @@
+const assert = require('chai').assert;
+//const sayHello = require('../app').sayHello;
+//const multiplyNumbers = require('../app').multiplyNumbers;
+const app = require('../app');
+
+sayHelloResult = app.sayHello();
+multiplyNumbersResult = app.multiplyNumbers(3, 2);
+
+describe('App', function(){
+        describe('sayHello()', function(){
+            it('should say hello world', function(){
+     //         let result = sayHello();
+            assert.equal(sayHelloResult, 'Hello World');
+            });
+
+            it('should return type String', function(){
+    //       let result = sayHello();
+             assert.typeOf(sayHelloResult, 'string');
+            });
+        });
+        describe('multiplyNumbers()', function(){
+             it('should multiply correctly', function(){
+    //          let result = multiplyNumbers(3, 2);
+            assert.isAtLeast(multiplyNumbersResult, 5);
+         });
+    });
+});
